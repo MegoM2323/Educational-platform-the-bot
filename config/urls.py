8a39d9571100cfb14fe95028a1f28e53a651c10a
MegoM2323 <your-email@example.com>
2025-10-21 18:19:32 +0300

    Рабочая платежка через ЮКасса

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from payments.views import pay_page, notify, pay_success, pay_fail
+from payments.views import pay_page, yookassa_webhook, pay_success, pay_fail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", pay_page, name="pay_page"),
-    path("payments/notify/", notify, name="payments_notify"),
+    path("yookassa-webhook", yookassa_webhook, name="yookassa_webhook"),
     path("payments/success/", pay_success, name="payments_success"),
     path("payments/fail/", pay_fail, name="payments_fail"),
 ]

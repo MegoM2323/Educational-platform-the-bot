c7fdc46e77e8ba4115a6e84f8477af42fb5d0d95
MegoM2323 <your-email@example.com>
2025-10-21 18:56:41 +0300

    Исправлен баг в успешной оплаты

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from payments.views import pay_page, yookassa_webhook, pay_success, pay_fail
+from payments.views import pay_page, yookassa_webhook, pay_success, pay_fail, check_payment_status
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("yookassa-webhook", yookassa_webhook, name="yookassa_webhook"),
     path("payments/success/", pay_success, name="payments_success"),
     path("payments/fail/", pay_fail, name="payments_fail"),
+    path("api/check-payment/", check_payment_status, name="check_payment_status"),
 ]
